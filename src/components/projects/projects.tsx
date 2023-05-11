a00983df13b5a6f86cbf19d26eb2b11e07d49267
Aaron <aarongraybill3@gmail.com>
2023-05-11 15:20:52 -0500

    feat: responsive layout for the projects section

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -47,6 +47,27 @@ const Projects = () => {
                         <motion.a className='project-button' whileHover={projectHover}>Learn More</motion.a>
                     </motion.div>
                 </motion.div>
+                <motion.div whileHover={projectHover} className='project'>
+                    <div className='project-title'>
+                        Smoothy
+                    </div>
+                    <img className='project-img' src='https://github.com/y0Phoenix/Smoothy/blob/development/pictures/Smoothy%20Logo.png?raw=true'></img>
+                    <motion.div className='project-hover' initial={{opacity: 0}} whileHover={{opacity: 1}}>
+                        <p className='project-text'>A feature rich Discord Music Bot built with Node and Discordjs</p>
+                        <motion.a className='project-button' whileHover={projectHover}>Learn More</motion.a>
+                    </motion.div>
+                </motion.div>
+                <motion.div whileHover={projectHover} className='project'>
+                    <div className='project-title'>
+                        Smoothy
+                    </div>
+                    <img className='project-img' src='https://github.com/y0Phoenix/Smoothy/blob/development/pictures/Smoothy%20Logo.png?raw=true'></img>
+                    <motion.div className='project-hover' initial={{opacity: 0}} whileHover={{opacity: 1}}>
+                        <p className='project-text'>A feature rich Discord Music Bot built with Node and Discordjs</p>
+                        <motion.a className='project-button' whileHover={projectHover}>Learn More</motion.a>
+                    </motion.div>
+                </motion.div>
+
             </div>
         </motion.div>
     )
