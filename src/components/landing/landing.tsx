b79a09eca4f2e3fb8259f4ba2916f78ba82da7ef
Aaron <aarongraybill3@gmail.com>
2025-02-19 19:41:11 -0600

    feat: removed video from webpage lol

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -3,8 +3,8 @@ import { motion } from 'framer-motion';
 import { headerDelay, headerDelayInc, noShow, sectionNoShow, sectionShow, sectionShowTransition, show, } from '../configs/motion';
 
 const Landing = () => {
-    // const header = `Hi 👋  I'm Aaron. I am a Certified Full Stack Engineer 🧑‍💻  Please Enjoy my Website 🙂`;
-    const header = `Hi I'm Aaron Graybill. I am an MIT Certified Full Stack Engineer Please Enjoy my Website`;
+    const header = `Hi 👋  I'm Aaron. I am a Certified Full Stack Engineer 🧑‍💻  Please Enjoy my Website 🙂`;
+    // const header = `Hi I'm Aaron Graybill. I am an MIT Certified Full Stack Engineer Please Enjoy my Website`;
 
     const fadedHeader: JSX.Element[] = [];
     let emoji = "";
@@ -22,18 +22,21 @@ const Landing = () => {
     // if there was an emoji at the end of the string
     if (emoji.length > 0) fadedHeader.push(<motion.span key={header.length} initial={noShow} animate={show} transition={{ delay: headerDelay.delay + (header.length * headerDelayInc) }}>{emoji}</motion.span>);
 
+    //! old personal video I made for school don't use lol
+    // const video = <motion.div initial={sectionNoShow} whileInView={sectionShow} transition={sectionShowTransition} viewport={{ once: true }} className='video-main-container'>
+    //     <div className='video-main section'>
+    //         <div className="video-container">
+    //             <iframe className='video' /** width={videoRes.width} height={videoRes.height} **/ src="https://www.youtube.com/embed/NqfQgthE0O8" title="YouTube video player"
+    //                 frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>
+    //             </iframe>
+    //         </div>
+    //     </div>
+    // </motion.div>;
+
     return (
         <div className='landing-main' id="landing">
             <h1 id="landing-header" className='header landing-header'>{fadedHeader}</h1>
-            <motion.div initial={sectionNoShow} whileInView={sectionShow} transition={sectionShowTransition} viewport={{ once: true }} className='video-main-container'>
-                <div className='video-main section'>
-                    <div className="video-container">
-                        <iframe className='video' /** width={videoRes.width} height={videoRes.height} **/ src="https://www.youtube.com/embed/NqfQgthE0O8" title="YouTube video player"
-                            frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>
-                        </iframe>
-                    </div>
-                </div>
-            </motion.div>
+            {/* {video} */}
         </div>
     )
 }
